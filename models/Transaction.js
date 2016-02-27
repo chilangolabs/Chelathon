efc8b8a948a7db41ff7b9cac6f687b14cb085af7
Rodrigo Moreno <moreno.rdr@gmail.com>
2016-02-27 13:42:30 -0600

    + Transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var TransactionModel = function() {
+  var transactionSchema = new mongoose.Schema({
+    code: String,
+    status: String,
+    data: {},
+    orden: {},
+    address: {},
+    payment_method: String,
+    _user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User
+    },
+    _createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+  var transaction = mongoose.model('Transaction', transactionSchema);
+
+  return transaction;
+};
+
+module.exports = new TransactionModel();
