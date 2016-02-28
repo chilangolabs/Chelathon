22a7416a141c34ae9d9ff65943f74ef7f4da4c91
Christian Ceciliano <cristiancehi@gmail.com>
2016-02-28 10:50:36 -0600

    Ultimas lineas

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ exports.handler = function(event, context) {
       doc = new User({chatId: event.message.chat.id});
     }
     return promise.then(function() {
+      if (doc.status === 'more') {
+        if (event.message.text === 'Sí') {
+          doc.status = null;
+        }
+      }
       if (!doc.status) {
         doc.status = 'selection';
         doc.saveAsync().then(function() {
@@ -59,17 +64,97 @@ exports.handler = function(event, context) {
       }
       switch (doc.status) {
         case 'selection':
-
-          break;
-        default:
-
+          doc.order.selection = event.message.text;
+          doc.status = 'quantity';
+          return Promise.all([
+            telegramAPI('sendMessage', {
+              chat_id: event.message.chat.id,
+              text: '¿Cuántas vas a querer?',
+              reply_markup: {
+                hide_keyboard: true
+              }
+            }),
+            doc.saveAsync()
+          ]);
+        case 'quantity':
+          doc.order.cart = doc.order.cart || [];
+          doc.order.cart.push({
+            type: doc.order.selection,
+            quantity: event.message.text
+          });
+          doc.status = 'more';
+          return Promise.all([
+            telegramAPI('sendMessage', {
+              chat_id: event.message.chat.id,
+              text: '¿Quieres agregar algo más?',
+              reply_markup: {
+                keyboard: [
+                  ['Sí', 'No, ya envíamelo.']
+                ]
+              }
+            }),
+            doc.saveAsync()
+          ]);
+        case 'more':
+          doc.status = 'location';
+          return Promise.all([
+            telegramAPI('sendMessage', {
+              chat_id: event.message.chat.id,
+              text: 'Envíanos tu ubicación',
+              reply_markup: {
+                hide_keyboard: true
+              }
+            }),
+            doc.saveAsync()
+          ]);
+        case 'location':
+          doc.status = 'confirmation';
+          return Promise.all([
+            telegramAPI('sendMessage', {
+              chat_id: event.message.chat.id,
+              text: 'Confirmanos tu órden. ¿Deséas recibirla?',
+              reply_markup: {
+                keyboard: [
+                  ['Sí', 'Cancelar']
+                ]
+              }
+            }).then(function() {
+              var message = '';
+              (doc.order.cart || []).forEach(function(value, index, arr) {
+                message += value.type + ' x' + value.quantity;
+              });
+              return telegramAPI('sendMessage', {
+                chat_id: event.message.chat.id,
+                text: message,
+                reply_markup: {
+                  hide_keyboard: true
+                }
+              });
+            }),
+            doc.saveAsync()
+          ]);
+        case 'confirmation':
+          doc.status = null;
+          return Promise.all([
+            telegramAPI('sendMessage', {
+              chat_id: event.message.chat.id,
+              text: 'Uno de nuestros repartidores ha sido ' +
+                'asignado y va en camino.',
+              reply_markup: {
+                hide_keyboard: true
+              }
+            }),
+            doc.saveAsync()
+          ]);
       }
     });
   }).then(function(res) {
-    return '*success* ' + JSON.stringify(res);
   }).catch(function(err) {
     return '*fail* ' + JSON.stringify(err);
   }).then(function(text) {
+    if (!res) {
+      context.succeed(true);
+    }
     context.succeed({
       method: 'sendMessage',
       parse_mode: 'Markdown',
