88f5fbf330b06da79fa5a51543cb6fd4c209fb12
Sebastian <sebastian.tellez.o@gmail.com>
2016-02-28 01:24:48 -0600

    add recycler products

--- app/src/main/java/com/chilangolabs/modelonow/adapters/AdapterListProducts.java
+++ app/src/main/java/com/chilangolabs/modelonow/adapters/AdapterListProducts.java
@@ -2,12 +2,14 @@ package com.chilangolabs.modelonow.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.chilangolabs.modelonow.Entitys.ItemProducto;
+import com.chilangolabs.modelonow.R;
+import com.chilangolabs.modelonow.customwidgets.TextViewMontserrat;
 import com.like.LikeButton;
 
 import java.util.List;
@@ -20,34 +22,44 @@ public class AdapterListProducts extends RecyclerView.Adapter<AdapterListProduct
     List<ItemProducto> itemProductos;
     Context context;
 
-    public AdapterListProducts(List<ItemProducto> itemProductos, Context context) {
+    public AdapterListProducts(Context context, List<ItemProducto> itemProductos) {
         this.itemProductos = itemProductos;
         this.context = context;
     }
 
     @Override
     public ListProductosHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_products, parent, false);
+        return new ListProductosHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ListProductosHolder holder, int position) {
-
+        holder.txtName.setText(itemProductos.get(position).getNameBeer());
+        holder.txtDescrip.setText(itemProductos.get(position).getDetailBeerPack());
+        holder.txtPrice.setText(itemProductos.get(position).getPrice());
+        holder.likeButton.setLiked(itemProductos.get(position).isFavorite());
+        holder.imgProduct.setImageResource(itemProductos.get(position).getImgBeer());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemProductos.size();
     }
 
     public static class ListProductosHolder extends RecyclerView.ViewHolder {
 
-        TextView txtName, txtDescrip, txtPrice;
+        TextViewMontserrat txtName, txtDescrip, txtPrice;
         ImageView imgProduct;
         LikeButton likeButton;
 
         public ListProductosHolder(View itemView) {
             super(itemView);
+            txtName = (TextViewMontserrat) itemView.findViewById(R.id.itemTxtName);
+            txtDescrip = (TextViewMontserrat) itemView.findViewById(R.id.itemTxtDescrip);
+            txtPrice = (TextViewMontserrat) itemView.findViewById(R.id.itemTxtPrice);
+            imgProduct = (ImageView) itemView.findViewById(R.id.itemImgProduct);
+            likeButton = (LikeButton) itemView.findViewById(R.id.itemBtnHeart);
         }
     }
 
--- app/src/main/java/com/chilangolabs/modelonow/fragments/FragmentProductList.java
+++ app/src/main/java/com/chilangolabs/modelonow/fragments/FragmentProductList.java
@@ -3,14 +3,23 @@ package com.chilangolabs.modelonow.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.chilangolabs.modelonow.Entitys.ItemProducto;
 import com.chilangolabs.modelonow.R;
+import com.chilangolabs.modelonow.adapters.AdapterListProducts;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentProductList extends Fragment {
 
+    RecyclerView recyclerView;
+    AdapterListProducts adapterListProducts;
 
     public FragmentProductList() {
         // Required empty public constructor
@@ -20,8 +29,26 @@ public class FragmentProductList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_product_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_fragment_product_list, container, false);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.rcProducts);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+        adapterListProducts = new AdapterListProducts(getActivity(), getProducts());
+        recyclerView.setAdapter(adapterListProducts);
+        return rootView;
+    }
+
+    private List<ItemProducto> getProducts() {
+        List<ItemProducto> itemProductos = new ArrayList<>();
+        itemProductos.add(new ItemProducto("Bud Light", "12 Pack • Botella importada 12 Oz", "$140.00", R.mipmap.beer_stella, true));
+        itemProductos.add(new ItemProducto("Bud Light", "12 Pack • Botella importada 12 Oz", "$140.00", R.mipmap.beer_stella, true));
+        itemProductos.add(new ItemProducto("Bud Light", "12 Pack • Botella importada 12 Oz", "$140.00", R.mipmap.beer_stella, true));
+        itemProductos.add(new ItemProducto("Bud Light", "12 Pack • Botella importada 12 Oz", "$140.00", R.mipmap.beer_stella, true));
+        itemProductos.add(new ItemProducto("Bud Light", "12 Pack • Botella importada 12 Oz", "$140.00", R.mipmap.beer_stella, true));
+        itemProductos.add(new ItemProducto("Bud Light", "12 Pack • Botella importada 12 Oz", "$140.00", R.mipmap.beer_stella, true));
+        itemProductos.add(new ItemProducto("Bud Light", "12 Pack • Botella importada 12 Oz", "$140.00", R.mipmap.beer_stella, true));
+        itemProductos.add(new ItemProducto("Bud Light", "12 Pack • Botella importada 12 Oz", "$140.00", R.mipmap.beer_stella, true));
+        return itemProductos;
     }
 
 }
