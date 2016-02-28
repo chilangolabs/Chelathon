ec2e740cc5f7469a423061badbe0037dcafeca65
Christian Ceciliano <cristiancehi@gmail.com>
2016-02-28 10:10:35 -0600

    New: Flujo Inicial

--- index.js
+++ index.js
@@ -6,26 +6,17 @@ var Promise     = require('bluebird');
 var db          = require('./src/db');
 var telegramAPI = require('./src/api');
 var config      = require('./src/env');
+var User        = require('./model/User');
 
 exports.handler = function(event, context) {
   console.log('event', event);
   console.log('context', context);
-  var time = (function() {
-    var initial = context.getRemainingTimeInMillis();
-    return function(message) {
-      var diff = initial - context.getRemainingTimeInMillis();
-      console.log('time', message || '', diff);
-    };
-  })();
-  time('Handler start');
   if (event.webhook) {
     console.log('Setting Webhoook');
     telegramAPI('setWebhook', {url: config.APPLICATION_WEBHOOK})
     .then(function() {
-      time();
       context.succeed(true);
     }).catch(function(err) {
-      time();
       context.fail(err);
     });
     return;
@@ -33,21 +24,52 @@ exports.handler = function(event, context) {
   if (!event.message) {
     return context.fail('Evento no reconocido.');
   }
-  var getUser = db.getUser(event.message.chat);
-  // Si no es ningún mensaje
-  return telegramAPI('forwardMessage', {
-    chat_id: event.message.chat.id,
-    from_chat_id: event.message.chat.id,
-    message_id: event.message.message_id
-  }).then(function() {
-    time('Response forwardMessage');
-    return getUser;
+  var getUser = User.findOneAsync({chatId: event.message.chat.id})
+  .then(function(doc) {
+    var promise = Promise.resolve();
+    if (!doc) {
+      doc = new User({chatId: event.message.chat.id});
+    }
+    return promise.then(function() {
+      if (!doc.status) {
+        doc.status = 'selection';
+        doc.saveAsync().then(function() {
+          return telegramAPI('sendMessage', {
+            chat_id: event.message.chat.id,
+            text: 'Bienvenido a Modelo Now Bot\n' +
+            'La nueva forma sencilla de pedir tus cheves.',
+          });
+        }).then(function() {
+          return telegramAPI('sendMessage', {
+            chat_id: event.message.chat.id,
+            text: '¿Qué deseas comprar?',
+            reply_markup: {
+              keyboard: [
+                ['Bud Light', 'Corona Extra'],
+                ['Michelob Ultra', 'Modelo Especial'],
+                ['Corona Light', 'Modelo Especial'],
+                ['Stella Artois', 'Barrilito'],
+                ['Bocanegra Dunkel', 'Bocanegra Pilsner']
+              ]
+            }
+          });
+        }).then(function() {
+          return doc;
+        });
+      }
+      switch (doc.status) {
+        case 'selection':
+
+          break;
+        default:
+
+      }
+    });
   }).then(function(res) {
     return '*success* ' + JSON.stringify(res);
   }).catch(function(err) {
     return '*fail* ' + JSON.stringify(err);
   }).then(function(text) {
-    time('Response DynamoDb');
     context.succeed({
       method: 'sendMessage',
       parse_mode: 'Markdown',
@@ -55,7 +77,8 @@ exports.handler = function(event, context) {
       text: text
     });
   }).catch(function(err) {
-    time('Fail');
     context.fail(err);
   });
+  // Si no es ningún mensaje
+
 };
