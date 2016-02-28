95ee8e160066fdc6e9d32a6da4fef89e141e4279
Sebastian <sebastian.tellez.o@gmail.com>
2016-02-28 03:18:56 -0600

    add functionality to custom dialog

--- app/src/main/java/com/chilangolabs/modelonow/adapters/AdapterListProducts.java
+++ app/src/main/java/com/chilangolabs/modelonow/adapters/AdapterListProducts.java
@@ -1,12 +1,16 @@
 package com.chilangolabs.modelonow.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.chilangolabs.modelonow.Entitys.ItemProducto;
 import com.chilangolabs.modelonow.R;
 import com.chilangolabs.modelonow.customwidgets.TextViewMontserrat;
@@ -21,6 +25,7 @@ public class AdapterListProducts extends RecyclerView.Adapter<AdapterListProduct
 
     List<ItemProducto> itemProductos;
     Context context;
+    MaterialDialog dialog;
 
     public AdapterListProducts(Context context, List<ItemProducto> itemProductos) {
         this.itemProductos = itemProductos;
@@ -40,6 +45,44 @@ public class AdapterListProducts extends RecyclerView.Adapter<AdapterListProduct
         holder.txtPrice.setText(itemProductos.get(position).getPrice());
         holder.likeButton.setLiked(itemProductos.get(position).isFavorite());
         holder.imgProduct.setImageResource(itemProductos.get(position).getImgBeer());
+        dialog = new MaterialDialog.Builder(context)
+                .customView(R.layout.content_dialog_product, true)
+                .positiveText("Añadir al carrito")
+                .positiveColor(context.getResources().getColor(R.color.colorPrimary))
+                .negativeText("Cancelar")
+                .build();
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+
+        Button btnMinus = (Button) dialog.findViewById(R.id.dialogBtnMinus);
+        Button btnPlus = (Button) dialog.findViewById(R.id.dialogBtnPlus);
+        TextViewMontserrat txtName = (TextViewMontserrat) dialog.findViewById(R.id.dialogTxtName);
+        final TextViewMontserrat txtNumerPack = (TextViewMontserrat) dialog.findViewById(R.id.dialogTxtNumber);
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number = Integer.parseInt(txtNumerPack.getText().toString());
+                number++;
+                txtNumerPack.setText(number + "");
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number = Integer.parseInt(txtNumerPack.getText().toString());
+                if (number > 0) {
+                    number--;
+                    txtNumerPack.setText(number + "");
+                }
+            }
+        });
+
     }
 
     @Override
@@ -52,6 +95,7 @@ public class AdapterListProducts extends RecyclerView.Adapter<AdapterListProduct
         TextViewMontserrat txtName, txtDescrip, txtPrice;
         ImageView imgProduct;
         LikeButton likeButton;
+        CardView cardView;
 
         public ListProductosHolder(View itemView) {
             super(itemView);
@@ -60,6 +104,7 @@ public class AdapterListProducts extends RecyclerView.Adapter<AdapterListProduct
             txtPrice = (TextViewMontserrat) itemView.findViewById(R.id.itemTxtPrice);
             imgProduct = (ImageView) itemView.findViewById(R.id.itemImgProduct);
             likeButton = (LikeButton) itemView.findViewById(R.id.itemBtnHeart);
+            cardView = (CardView) itemView.findViewById(R.id.itemCardProduct);
         }
     }
 
